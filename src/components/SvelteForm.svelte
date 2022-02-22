368f9937419e1ec1bf85b108dd109a86d0747f3e
Andre Siedersleben <andre.siedersleben@protonmail.com>
2022-02-22 18:03:02 +0100

    Change: Plugin switch to svelte-form

--- src/components/SvelteForm.svelte
+++ src/components/SvelteForm.svelte
@@ -14,40 +14,37 @@ import TextInput from './SvelteFormInputField.svelte';
 
 // Please see the opts below.
 const formOpts = {
-  subscribe: true,
-  onCreateValidation: false,
+  //subscribe: true,
+  //onCreateValidation: false,
 };
 
 const form = createForm(
   {
-    username: '',
-    password: '',
+    title: '',
   },
   {
-    username: [
+    title: [
       required('This field is required'),
       email('Invalid email format')
     ],
-    password: required('This field is required'),
   }
 );
 
 // directly subscribe to form state change
-form.subscribe((f) => console.log(f.valid));
+//form.subscribe((f) => console.log(f.valid));
 
 // or use shorthand access
 console.log($form.valid)
 
 // get the current form data
 console.log('Form Data: ', form.data())
-
-function onInput (event) {
-  console.log('Textinput: ', event);
-  form.validate();
-}
+form.validate();
 </script>
 
 <TextInput
   form={form}
-  name='title'
-  on:input={ onInput } />
+  name="title" />
+
+<TextInput
+  form={form}
+  name="url" />
--- src/components/SvelteFormInputField.svelte
+++ src/components/SvelteFormInputField.svelte
@@ -1,29 +1,39 @@
 <script>
-  // Props
   export let form;
   export let name;
 
-  // Get the form field
-  $: field = form.field(name);
-  // Value svelte store
-  $: value = field.value;
-  // State svelte store, {valid: boolean, error: string}
-  $: state = field.state;
+  $: field = form?.field(name);
+  $: value = form?.field(name)?.value;
+  $: state = field?.state; // example: { valid: false, error: 'Field is invalid!' }
+
+  /*function onInput (event) {
+    console.log('onInput: ', event);
+    form.validate();
+    
+    // on:input={onInput}
+  }*/
 </script>
 
-<div class="input" class:error="{$state.valid === false}">
-  <input type="text" bind:value="{$value}" />
-  <p class="validation">{$state.error}</p>
+<div class="input" class:error="{$state?.valid === false}">
+  <input type="text" name="{name}" bind:value="{$value}" />
+  <p class="validation">{$state?.error}</p>
 </div>
 
 <style lang="postcss">
-/*
-.input .validation {
+div {
+  font-family: "'Open Sans', 'Helvetica Neue', sans-serif"; 
+}
+
+input + .validation {
   display: none;
 }
   
-input.error .validation {
+.error .validation {
   display: block;
 }
-*/
+
+.error {
+  color: tomato;
+  font-weight: bolder;
+}
 </style>
