2f6a5e06da6bf6881f426a1441efd4df0c393c92
jsbase <andre.siedersleben@gmail.com>
2022-02-23 15:07:32 +0100

    1 file added

---
+++ src/components/JsonTree.svelte
@@ -0,0 +1,16 @@
+<script>
+  import JSONTree from 'svelte-json-tree'
+    // your json data to view
+      const value = {
+          array: [1, 2, 3],
+              bool: true,
+                  object: {
+                        foo: 'bar'
+                            },
+                            		symbol: Symbol('foo'),
+                                		nested: [{
+                                    			a: [1, '2', null, undefined]
+                                          		}]
+                                                }
+                                                </script>
+                                                <JSONTree {value} />
