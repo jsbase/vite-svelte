984bc4c5e94c3d8d48e61380a2ac50d7872b83b5
jsbase <andre.siedersleben@gmail.com>
2022-02-24 00:07:28 +0100

    3 files modified

--- src/components/SvelteJsonTree.svelte
+++ src/components/SvelteJsonTree.svelte
@@ -1,6 +1,6 @@
 <script>
-import JsonTree from 'svelte-json-tree'
-import * as Routers from '../../db.json'
+import JsonTree from 'svelte-json-tree';
+// import * as Routers from '../../db.json';
 
 const emptyData = {
 	title: '',
@@ -8,7 +8,17 @@ const emptyData = {
 	tags: [],
 };
 
-const value = Routers.routers || emptyData;
+// render with vite backend
+// const value = Routers.routers || emptyData;
+
+import { onMount } from 'svelte';
+
+	let photos = [];
+
+	onMount(async () => {
+		const res = await fetch(`https://jsonplaceholder.typicode.com/photos?_limit=20`);
+		photos = await res.json();
+	});
 </script>
 
 <JsonTree value={value} />
