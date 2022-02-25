9c112eb371b67bc6e9bb3cbdff9ae03b8bb1d832
jsbase <andre.siedersleben@gmail.com>
2022-02-25 02:11:10 +0100

    Change: components

--- src/App.svelte
+++ src/App.svelte
@@ -8,6 +8,12 @@ import "./index.css";
 
 <header>
   <Logo />
+
+  <div class="text-5xl font-extrabold">
+    <span class="bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500">
+      lancom help center
+    </span>
+  </div>
 </header>
 
 <main>
